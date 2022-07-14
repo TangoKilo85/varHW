330965eda5db9ef27f1e57230cfc654703f98e71
Tyler Wellman <TangoKilo1985@gmail.com>
2022-07-14 08:06:53 -0400

    Gitz it

---
+++ test.js
@@ -0,0 +1,37 @@
+var a = 1;
+var b = 2;
+var c = 3;
+var d = 4;
+var e = 5;
+var f = 6;
+var g = 7;
+var h = 8;
+var i = 9;
+
+var result = b + c;
+console.log(result);
+
+var result = c + f;
+console.log(result);
+
+var result = h - f;
+console.log(result);
+
+var result = i - g;
+console.log(result);
+
+var result = d * a;
+console.log(result);
+
+var result = e * f;
+console.log(result);
+
+var result = i / c;
+console.log(result);
+
+var result = g / d;
+console.log(result);
+
+console.log(++i);
+
+console.log(--f);
